fbea2f3bfd3a0daf24a7a0d51922a325b5b180ae
Stiven Valencia <stiven.valencia1@utp.edu.co>
2022-08-31 13:24:38 -0500

    new server 0.0

--- Server/server.py
+++ Server/server.py
@@ -9,6 +9,21 @@ import json
 1: Ya existe el archivo
 '''
 
+def iniciar_server():
+    if os.path.exists("server_data.json"):
+        with open("server_data.json") as f:
+            data = json.load(f)
+            print("data loaded")
+        return data
+    else:
+        data = {}
+        with open("server_data.json", "w") as newf:
+            json.dump(data, newf, indent=3)
+            print("data loaded")
+        return data
+
+
+
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
@@ -16,52 +31,5 @@ socket.bind("tcp://*:5555")
 while True:
     #  Wait for next request from client
     #se lee el archivo que contiene los hash de documentos almacenados en server
-    with open("files.json") as files:
-        data = json.load(files)
-    #ser recibe el mensaje del cliente con el hash del archivo
-    info = socket.recv_multipart()
-    md5_hash = info[0].decode()
-    name_client = info[1].decode()
-    name_file = info[2].decode()
-    #se verifica que la llave exista en el diccionario
-    if md5_hash in data.keys() :
-        if name_client in data[md5_hash].keys():
-            socket.send_string("1")
-        else:
-            data[md5_hash].update({name_client: name_file})
-            with open("files.json", "w") as newfiles:
-                json.dump(data, newfiles, indent=4)
-            socket.send_string("0")
-    else:
-        socket.send_string(" ")
-        #se recibe la informacion que va usar el cliente para crear el archivo por primera vez
-        message = socket.recv_multipart()
-        name_client1 = message[0].decode()
-        name_file1 = message[1].decode()
-        size_file1 = message[3].decode()
-        dm5_hash1 = message[4].decode()
-
-        with open(dm5_hash1,"ab") as f:
-            f.write(message[2])
-            f.close()
-
-        while os.path.getsize(dm5_hash1) < int(size_file1):
-            socket.send_string(" ")
-            message = socket.recv_multipart()
-            name_client1 = message[0].decode()
-            name_file1 = message[1].decode()
-            size_file1 = message[3].decode()
-            dm5_hash1 = message[4].decode()
-
-            with open(dm5_hash1,"ab") as f:
-                f.write(message[2])
-                f.close()
-
-        socket.send_string("0")
-        with open("files.json") as files1:
-            data1 = json.load(files1)
-        #data1[dm5_hash1]
-        data1[dm5_hash1]={name_client1: name_file1}
-        with open("files.json", "w") as newfiles:
-            json.dump(data1, newfiles, indent=4)
-        print(name_client+": Archivo recibido")
+    data_server = iniciar_server()
+    
