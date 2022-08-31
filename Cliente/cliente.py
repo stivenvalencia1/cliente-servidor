57fc72d148ce2dd2938b466a06d2091f044e2bdc
Stiven Valencia <stiven.valencia1@utp.edu.co>
2022-08-31 16:38:02 -0500

    cliente y server nuevo v0.1, un solo multipart

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -70,29 +70,5 @@ if info_file:
                 print("Se ha enviado todo el contenido ")
                 break
 
-
-    """socket.send_multipart()
-    #se recibe la respuesta del server para saber si existe el archivo o no
-    message = socket.recv().decode()
-    if message == "1":
-        print(f"Received reply result: Ya cuenta con un archivo con ese nombre")
-    elif message == "0":
-        print(f"Received reply result: Archivo recibido")
-    elif message == " ":
-        print("...")
-        with open (name_file,"rb") as f:
-            contenido = f.read(1024)
-            while contenido:
-                socket.send_multipart([bytes(name_client.encode()), bytes(name_file.encode()), contenido, bytes(str(size_file).encode()), bytes(dm5_hash.encode())])
-                contenido=f.read(1024)
-                message = socket.recv()
-                if message.decode() == "0":
-                    print(f"Received reply result: Archivo recibido")
-                    break
-                elif message.decode() == "1" :
-                    print(f"Received reply result: Ya cuenta con un archivo con ese nombre")
-                    break
-        f.close()"""
-
 else:
     print("Archivo no encontrado, por favor revise el nombre del archivo")
